70c153d5eeddf43010efa153f7a4e4fe580cb704
int0h <int0h@yandex.ru>
2018-03-04 14:48:19 +0300

    customizable showd item count

--- src/app.tsx
+++ src/app.tsx
@@ -6,10 +6,17 @@ class App extends Component<{}> {
     clickTimes = new HyperValue([] as number[]);
     lastClickTime = this.hs.auto(() => this.clickTimes.$.slice(-1)[0] || 'n/a');
 
-    // here we are creating a hyper-value array
-    // which is always equal to 5 latests items
-    // of clickItems array
-    showedLog = this.hs.slice(this.clickTimes, -5);
+    // it's a hyper-value represnting the quantity
+    // of items to show
+    showItems = new HyperValue(5);
+
+    // it's a negative value of showItems
+    // which is needed to slice array from the end
+    sliceStart = this.hs.auto(() => this.showItems.$ > 0 ? -this.showItems.$ : 0);
+
+    // now showedLog is always equal to
+    // 'showItems' last elements of clickItems
+    showedLog = this.hs.slice(this.clickTimes, this.sliceStart);
 
     handleClick = () => {
         this.hs.insert(this.clickTimes, Infinity, Date.now());
@@ -21,6 +28,7 @@ class App extends Component<{}> {
             <button onClick={this.handleClick}>Click me!</button>
             <div>
                 <h5>Log:</h5>
+                Show last <input type='number' value={this.showItems}/> entries:
                 <ol>{
                     this.hs.map(this.showedLog, item => {
                         const dateString = (new Date(item)).toString();
