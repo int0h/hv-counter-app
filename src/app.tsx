63777f72b4ae4cd56132e408203aa293b6d6a204
int0h <int0h@yandex.ru>
2018-03-04 14:30:06 +0300

    click count -> timpstamp

--- src/app.tsx
+++ src/app.tsx
@@ -3,22 +3,30 @@ import {jsx, Component} from 'hv-jsx';
 import {renderIn} from 'hv-dom';
 
 class App extends Component<{}> {
-    // we are creating a "hyper-value" here,
-    // it's not required to be a class property
-    // it can be any variable in any place in your code
-    // 0 - is the initial value of this hyper-value
-    count = new HyperValue(0);
+    // now instead of count we have an array of
+    // stamps wrapped in hyper-value
+    // note: [] as number[] - is a way to tell typescript
+    // that it's not any[] kind of array
+    clickTimes = new HyperValue([] as number[]);
+
+    // it's a hyper-value which is always equal to
+    // the last element of the array hyper-value above
+    // note: [...].slice(-1)[0] - is a bit wierd but short way
+    // to get the last item of an array
+    lastClickTime = this.hs.auto(() => this.clickTimes.$.slice(-1)[0] || 'n/a');
+
+    // it's the new click handler
+    // hs.insert - insert an element into hyper-value array
+    // on "Infinity" position
+    // (which is always the end of array)
+    handleClick = () => {
+        this.hs.insert(this.clickTimes, Infinity, Date.now());
+    }
 
     render() {
-        // here we're doing 2 things:
-        // 1) we output count hyper-value
-        // right into the document
-        // 2) we handle button click
-        // via increasing count value
-
         return <div>
-            <span>Click amount: {this.count}</span>
-            <button onClick={() => this.count.$++}>Click me!</button>
+            <span>Last click time: {this.lastClickTime}</span>
+            <button onClick={this.handleClick}>Click me!</button>
         </div>;
     }
 }
