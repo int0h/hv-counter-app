dde65d160ee2223ee2ba7560739e216ed821b38d
int0h <int0h@yandex.ru>
2018-03-04 14:27:32 +0300

    counting clicks

--- src/app.tsx
+++ src/app.tsx
@@ -3,10 +3,22 @@ import {jsx, Component} from 'hv-jsx';
 import {renderIn} from 'hv-dom';
 
 class App extends Component<{}> {
+    // we are creating a "hyper-value" here,
+    // it's not required to be a class property
+    // it can be any variable in any place in your code
+    // 0 - is the initial value of this hyper-value
+    count = new HyperValue(0);
+
     render() {
+        // here we're doing 2 things:
+        // 1) we output count hyper-value
+        // right into the document
+        // 2) we handle button click
+        // via increasing count value
+
         return <div>
-            <span>Click amount: 0</span>
-            <button>Click me!</button>
+            <span>Click amount: {this.count}</span>
+            <button onClick={() => this.count.$++}>Click me!</button>
         </div>;
     }
 }
