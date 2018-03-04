8032d63d7067818c95578ff570976f4d7acaa58e
int0h <int0h@yandex.ru>
2018-03-04 14:34:27 +0300

    added log

--- src/app.tsx
+++ src/app.tsx
@@ -3,22 +3,14 @@ import {jsx, Component} from 'hv-jsx';
 import {renderIn} from 'hv-dom';
 
 class App extends Component<{}> {
-    // now instead of count we have an array of
-    // stamps wrapped in hyper-value
-    // note: [] as number[] - is a way to tell typescript
-    // that it's not any[] kind of array
     clickTimes = new HyperValue([] as number[]);
-
-    // it's a hyper-value which is always equal to
-    // the last element of the array hyper-value above
-    // note: [...].slice(-1)[0] - is a bit wierd but short way
-    // to get the last item of an array
     lastClickTime = this.hs.auto(() => this.clickTimes.$.slice(-1)[0] || 'n/a');
 
-    // it's the new click handler
-    // hs.insert - insert an element into hyper-value array
-    // on "Infinity" position
-    // (which is always the end of array)
+    // here we are creating a hyper-value array
+    // which is always equal to 5 latests items
+    // of clickItems array
+    showedLog = this.hs.slice(this.clickTimes, -5);
+
     handleClick = () => {
         this.hs.insert(this.clickTimes, Infinity, Date.now());
     }
@@ -27,6 +19,15 @@ class App extends Component<{}> {
         return <div>
             <span>Last click time: {this.lastClickTime}</span>
             <button onClick={this.handleClick}>Click me!</button>
+            <div>
+                <h5>Log:</h5>
+                <ol>{
+                    this.hs.map(this.showedLog, item => {
+                        const dateString = (new Date(item)).toString();
+                        return <li>{dateString}</li>;
+                    })
+                }</ol>
+            </div>
         </div>;
     }
 }
